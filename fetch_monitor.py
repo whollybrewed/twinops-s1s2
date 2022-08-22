640c8410671846b0d06c4ddd6c267ed6b2989c39
whollybrewed <andylee47695@gmail.com>
2022-08-22 15:40:58 +0200

    add s2

--- fetch_monitor.py
+++ fetch_monitor.py
@@ -19,9 +19,9 @@ inkbirdOffsetNow = consumer.end_offsets([inkbirdTP]).get(inkbirdTP) - 1
 tiltOffsetNow = consumer.end_offsets([tiltTP]).get(tiltTP) - 1
 
 oms = OMSimulator()
-model, status = oms.importFile("s1.ssp")
+model, status = oms.importFile("s1s2.ssp")
 logger.info("Import SSP: %s", Types.Status(status))
-oms.newResources("s1.root:root.ssv")
+oms.newResources("s1s2.root:root.ssv")
 
 for message in consumer:
     # messageTime = datetime.fromtimestamp(int(message.timestamp)/1000).replace(microsecond=0) 
@@ -29,27 +29,28 @@ for message in consumer:
         if message.topic == 'inkbird2' and message.offset == inkbirdOffsetNow:
             dictInkbird = json.loads(message.value.decode())
             tempInkbird = dictInkbird.get('temp') + 273.15
-            oms.setReal('s1.Root.Temperature.T_amb', tempInkbird)
+            oms.setReal('s1s2.Root.Temperature.T_amb', tempInkbird)
             logger.info("Get T_amb (Temperature): %s", Types.Status(status))
-            oms.setReal('s1.Root.HeatTransfer.Tamb', tempInkbird)
+            oms.setReal('s1s2.Root.HeatTransfer.Tamb', tempInkbird)
             logger.info("Get T_amb (HeatTransfer): %s", Types.Status(status))
         if message.topic == 'tilt2' and message.offset == tiltOffsetNow:
             dictTilt = json.loads(message.value.decode())
             tempTilt = dictTilt.get('temp') + 273.15
             sgTilt = dictTilt.get('gravity')
-            oms.setReal('s1.Root.Temperature.T_0', tempTilt)
+            oms.setReal('s1s2.Root.Temperature.T_0', tempTilt)
+            oms.setReal('s1s2.Root.control_model.T_sensor', tempTilt)
             logger.info("Get T_0: %s", Types.Status(status))
-            oms.setReal('s1.Root.Temperature.sg', sgTilt)
+            oms.setReal('s1s2.Root.Temperature.sg', sgTilt)
             logger.info("Get SG: %s", Types.Status(status))
     except:
         pass
 
-status = oms.export(model, "s1.ssp")
+status = oms.export(model, "s1s2.ssp")
 logger.info("Update SSP: %s", Types.Status(status))
 
 repo = Repo(".")
 logger.info("Local repository: %s", repo)
-adds = repo.index.add(['s1.ssp'])
+adds = repo.index.add(['s1s2.ssp'])
 logger.info("Add: %s", adds)
 commit = repo.index.commit("New fetched data")
 logger.info("Commit: %s", commit)
--- init_models.py
+++ init_models.py
@@ -3,23 +3,25 @@ import json
 import requests
 
 oms = OMSimulator()
-model, status = oms.importFile("s1.ssp")
+model, status = oms.importFile("s1s2.ssp")
 print(Types.Status(status))
 
 oms.setResultFile(model, "results.csv", bufferSize=10)
 
 oms.instantiate(model)
-oms.setReal('s1.Root.Reaction_model.Temperature', 299.15)
-oms.setReal('s1.Root.Reaction_model.grams_yeast', 11.5)
-oms.setReal('s1.Root.Reaction_model.sg_0', 1.054)
-oms.setReal('s1.Root.Reaction_model.batch_volume', 20)
+oms.setReal('s1s2.Root.Reaction_model.Temperature', 299.15)
+oms.setReal('s1s2.Root.Reaction_model.grams_yeast', 11.5)
+oms.setReal('s1s2.Root.Reaction_model.sg_0', 1.054)
+oms.setReal('s1s2.Root.Reaction_model.batch_volume', 20)
+oms.setReal('s1s2.Root.control_model.T_set', 22)
+oms.setReal('s1s2.Root.control_model.T_sensor', 19)
 
 
 oms.initialize(model)
 oms.simulate(model)
 url = "https://b3de-104-28-250-40.eu.ngrok.io/pump"
 headers = {'Content-type': 'application/json'}
-command = {"command": int(oms.getReal('s1.Root.control_model.control_signal')[0])}
+command = {"command": int(oms.getReal('s1s2.Root.control_model.control_signal')[0])}
 r = requests.post(url, data=json.dumps(command), headers=headers)
 oms.terminate(model)
 oms.delete(model)
